91f77c889c96123937380b824067ef98089e3704
etorresh <me@etorresh.com>
2022-05-19 16:40:21 -0600

    set up environment files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { DailyWeatherComponent } from './daily-weather/daily-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailyWeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
