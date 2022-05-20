c62c405079ab5871897d9d135e64c642e513401a
etorresh <me@etorresh.com>
2022-05-19 22:13:41 -0600

    weather fetch waits for location to load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,30 +16,34 @@ export class AppComponent implements OnInit {
   constructor(private weather: WeatherService) {}
 
   ngOnInit() {
-    this.weather.getCurrentWeatherData().subscribe(currentWeather => {
-      const dailyData: DailyData = {
-        backgroundColor: "#dddddd",
-        time: String(this.date.toLocaleString('en-US', { hour: 'numeric', hour12: true})),
-        max: String(currentWeather.main.temp_max),
-        min: String(currentWeather.main.temp_min),
-        icon: `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`
-      }
-      this.weeklyData.push(dailyData);
-      // OpenWeatherMap doesn't offer daily forecasts for free. I'll use the 5 day forecast with 3 hour intervals.
-      this.weather.getFiveDayForecast().subscribe(forecast => {
-        for(let weather of forecast.list) {
-          this.date.setTime(weather.dt * 1000);
+    this.weather.getLocationLoaded().subscribe(value => {
+      if(value) {
+        this.weather.getCurrentWeatherData().subscribe(currentWeather => {
           const dailyData: DailyData = {
-            backgroundColor: "white",
+            backgroundColor: "#dddddd",
             time: String(this.date.toLocaleString('en-US', { hour: 'numeric', hour12: true})),
-            max: String(weather.main.temp_max),
-            min: String(weather.main.temp_min),
-            icon: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+            max: String(currentWeather.main.temp_max),
+            min: String(currentWeather.main.temp_min),
+            icon: `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`
           }
           this.weeklyData.push(dailyData);
-        }
-        this.loaded = true;
-      });
-    });
+          // OpenWeatherMap doesn't offer daily forecasts for free. I'll use the 5 day forecast with 3 hour intervals.
+          this.weather.getFiveDayForecast().subscribe(forecast => {
+            for(let weather of forecast.list) {
+              this.date.setTime(weather.dt * 1000);
+              const dailyData: DailyData = {
+                backgroundColor: "white",
+                time: String(this.date.toLocaleString('en-US', { hour: 'numeric', hour12: true})),
+                max: String(weather.main.temp_max),
+                min: String(weather.main.temp_min),
+                icon: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+              }
+              this.weeklyData.push(dailyData);
+            }
+            this.loaded = true;
+          });
+        });
+      }
+    })
   }
 }
--- src/weather.service.ts
+++ src/weather.service.ts
@@ -3,6 +3,7 @@ import { environment } from "./environments/environment";
 import { HttpClient } from "@angular/common/http";
 import {CurrentWeather} from "./interfaces/current-weather";
 import {ForecastWeather} from "./interfaces/forecast-weather";
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,17 @@ import {ForecastWeather} from "./interfaces/forecast-weather";
 export class WeatherService {
   lat = 51.049999;
   lon = -114.066666;
+  private locationLoaded: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   constructor(private http: HttpClient) {
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
+        this.locationLoaded.next(true);
+      }, () => {
+        this.lat = 51.049999;
+        this.lon = -114.066666;
+        this.locationLoaded.next(true);
       });
     }
   }
@@ -23,5 +30,8 @@ export class WeatherService {
   }
   getFiveDayForecast() {
     return this.http.get<ForecastWeather>(`${environment.weatherApiUrl}/forecast?lat=${this.lat}&lon=${this.lon}&cnt=4&units=metric&appid=${environment.weatherApiKey}`);
+  }
+  getLocationLoaded(): Observable<boolean> {
+    return this.locationLoaded.asObservable();
   }
 }
